583ccc6341893275b91e2c6e8aeee8279cb131f6
Terli1337 <adrian.terlecki1@gmail.com>
2023-01-01 22:15:56 +0100

    final

--- js/script.js
+++ js/script.js
@@ -1,23 +1,21 @@
 {
   console.log("Miłego sprawdzania mojej pracy domowej :)");
 
-const changeNameOfButton = (body) => {
-  const themeName = document.querySelector(".themeName");
+  const changeNameOfButton = (body) => {
+    const themeName = document.querySelector(".themeName");
 
-  if (body.classList.contains("dark")) 
-      themeName.innerText = "jasny";
-  else themeName.innerText = "ciemny";
-};
+    if (body.classList.contains("dark")) themeName.innerText = "jasny";
+    else themeName.innerText = "ciemny";
+  };
 
-const changeBackgroundColor = () => {
-  const button = document.querySelector(".body__button");
-  const body = document.querySelector(".body");
+  const changeBackgroundColor = () => {
+    const button = document.querySelector(".body__button");
+    const body = document.querySelector(".body");
 
-  button.addEventListener("click", () => {
-    body.classList.toggle("dark");
-    changeNameOfButton(body);
-  });
-
-};
-changeBackgroundColor();
+    button.addEventListener("click", () => {
+      body.classList.toggle("dark");
+      changeNameOfButton(body);
+    });
+  };
+  changeBackgroundColor();
 }
